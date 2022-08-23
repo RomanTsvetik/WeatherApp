7ebf9545a24c6e99bb765a4bb629ca369da8436b
Roman Tsvietik <zvetik85@gmail.com>
2022-08-23 17:40:54 +0300

    removed unnecessary imports

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Weather } from './weather.interface';
-import { map, Observable, tap, switchMap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
